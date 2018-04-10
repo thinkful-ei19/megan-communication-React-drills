da30ff528c916e8a6e7b51cba69af240997ed68c
Glitch (react-surprise-starting-point) <none>
2018-04-10 13:31:31 -0500

    drills complete

--- com-drill-1/src/components/surprise-button.js
+++ com-drill-1/src/components/surprise-button.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export default function SurpriseButton(props) {
-    return <button>Surprise!</button>;
+    
+    
+    return <button onCLick={props.onCLick}>Surprise!</button>;
 }
 
